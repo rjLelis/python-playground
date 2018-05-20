0c04e5c456814bf26cad18242a70c8eede0bfd42
R.J Lelis <renatojlelis@gmail.com>
2018-05-20 12:03:12 -0300

    update

--- guess_game.py
+++
@@ -1,8 +0,0 @@
-
-def game(guess, number):
-    if guess < number:
-        return -1 # guessed lower
-    elif guess > number:
-        return 1 # guessed higher
-    else:
-        return 0 # guessed correctly

9aea1cc638ce508077491106cc8c0dd1b3b41da1
R.J Lelis <renatojlelis@gmail.com>
2018-06-25 22:23:44 -0300

    Minor changes in the file

--- palindrome/palindrome.py
+++ palindrome/palindrome.py
@@ -1,9 +1,9 @@
 def remove_special_char(string):
     ''' Removes all special characters from a string '''
-    forbidden = (".", ",", "!", ";", ":", "-", "_", "?", "/")
+    forbidden_char = (".", ",", "!", ";", ":", "-", "_", "?", "/")
     new_string = ""
     for s in string:
-        if s not in forbidden:
+        if s not in forbidden_char:
             new_string += s
     return new_string
 
@@ -19,12 +19,12 @@ def palindrome_test(word):
 
 
 def main():
-    is_palindrome = \
-        palindrome_test(input("Type a word to test if it is palindrome: "))
+    word = input("Type a word to test if it is palindrome: ")
+    is_palindrome = palindrome_test(word)
     if is_palindrome:
-        print("The word typed is palindrome")
+        print(f"The word {word} is palindrome")
     else:
-        print("The word typed isn't palindrome")
+        print(f"The word {word} isn't palindrome")
 
 
 if __name__ == '__main__':

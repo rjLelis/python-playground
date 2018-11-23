40ad63bc0ca36d918d609ac357c53aaced25bb87
R.J Lelis <renatojlelis@gmail.com>
2018-11-23 14:46:21 -0200

    Update encryption.py

--- password-storage/encryption.py
+++ password-storage/encryption.py
@@ -2,7 +2,8 @@
 def caesar_cypher(message, key):
     '''Encrypts a message using the caesar cypher [WIP]'''
 
-    alphabet = ('a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i' ,'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+    alphabet = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i' , 'j', 'k', 'l', 'm', 
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
 
     encrypted = ''
 
@@ -12,6 +13,3 @@ def caesar_cypher(message, key):
         encrypted += alphabet[alphabet.index(m) + key]
 
     return encrypted
-
-
-print(caesar_cypher('renato', 1))

e5053fbd263d4090ffa9957fe83a229a744112f6
R.J Lelis <renatojlelis@gmail.com>
2019-04-14 21:39:34 -0300

    Rename file encrypt file

--- file-operations/file_encrypt.py
+++ file-operations/file_encrypt.py
@@ -4,11 +4,13 @@ from file_utils import get_file_content, store_file_content
 
 parser = argparse.ArgumentParser(description='Takes a file as argument and encrypts it')
 
-parser.add_argument('file', type=str)
-parser.add_argument('key', type=int)
+parser.add_argument('file', metavar='file to be encrypted', type=str)
+parser.add_argument('key', metavar='key to encrypt',type=int)
 
 args = parser.parse_args()
 
 file_content = get_file_content(args.file)
 
-store_file_content(encrypt(file_content, args.key), 'encriptado.txt')
+new_file = store_file_content(encrypt(file_content, args.key), 'encriptado.txt')
+
+print(f'content stored on {new_file}')
